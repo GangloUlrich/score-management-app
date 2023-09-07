400d87a6f7e2bc48117e57b8fe386ba54e9f9496
ulrich Ganglo <gangloulrichluc@gmail.com>
2023-09-07 23:56:46 +0100

    Set Team status after score to determine win,lost or draw

--- backend/app/Services/MatcheService.php
+++ backend/app/Services/MatcheService.php
@@ -4,35 +4,87 @@ namespace App\Services;
 
 use App\Http\Requests\MatcheFormRequest;
 use App\Models\Matche;
+use App\Models\Team;
 
 class MatcheService
 {
 
-    public  static function list(){
+    public static function list()
+    {
         $matches = Matche::all();
-        return response()->json($matches,200);
+        return response()->json($matches, 200);
     }
 
-    public  static function save(MatcheFormRequest $request){
+    public static function save(MatcheFormRequest $request)
+    {
         $matche = Matche::create($request->all());
-        return response()->json($matche,201);
+        return response()->json($matche, 201);
     }
 
-    public  static function update(MatcheFormRequest $request, $id){
+    public static function update(MatcheFormRequest $request, $id)
+    {
         $matche = Matche::find($id);
-        $matche->update(array_merge($request->all(),['status' => 'finished']));
+        $matche->update(array_merge($request->all(), ['status' => 'finished']));
 
-        return response()->json($matche,200);
+        if ($request->score_home > $request->score_away) {
+            self::updateWinner($request->home_team);
+            self::updateLost($request->away_team);
+        }
+
+        if ($request->score_home < $request->score_away) {
+            self::updateWinner($request->away_team);
+            self::updateLost($request->home_team);
+        }
+
+        if ($request->score_home == $request->score_away) {
+            self::updateDraw($request->away_team);
+            self::updateDraw($request->home_team);
+        }
+
+        return response()->json($matche, 200);
     }
 
-    public  static function show($id){
+    public static function updateWinner($teamId)
+    {
+        $team = Team::find($teamId);
+        $team->update([
+            'matches' => ($team->matches + 1),
+            'win' => ($team->win + 1),
+            'points' => ($team->points + 3),
+        ]);
+
+    }
+
+    public static function updateLost($teamId)
+    {
+        $team = Team::find($teamId);
+        $team->update([
+            'matches' => ($team->matches + 1),
+            'lost' => ($team->lost + 1),
+        ]);
+    }
+
+    public static function updateDraw($teamId)
+    {
+        $team = Team::find($teamId);
+        $team->update([
+            'matches' => ($team->matches + 1),
+            'draw' => ($team->matches + 1),
+            'points' => ($team->matches + 1)
+        ]);
+    }
+
+    public static function show($id)
+    {
         $matche = Matche::find($id);
-        return response()->json($matche,200);
+        return response()->json($matche, 200);
     }
-    public  static function delete($id){
+
+    public static function delete($id)
+    {
         $matche = Matche::find($id);
         $matche->delete();
-        return response()->json('',204);
+        return response()->json('', 204);
     }
 
 }
