f9a8bcb690ad23ff5784021226c48520820fe93e
ulrich Ganglo <gangloulrichluc@gmail.com>
2023-09-07 23:55:54 +0100

    Details match feature on Angular

--- frontend/src/app/main/components/details-match/details-match.component.ts
+++ frontend/src/app/main/components/details-match/details-match.component.ts
@@ -6,6 +6,7 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {MatcheService} from "../../../core/services/matche.service";
 import {AuthenticationService} from "../../../core/services/authentication.service";
 import {ActivatedRoute, Router} from "@angular/router";
+import {environment} from "../../../../environments/environment";
 
 @Component({
   selector: 'app-details-match',
@@ -15,16 +16,21 @@ import {ActivatedRoute, Router} from "@angular/router";
 export class DetailsMatchComponent implements OnInit,OnDestroy{
   public authUser: AuthUser = new AuthUser();
   private subscriptionAuthUser?: Subscription;
+  environment = environment
 
 
   public match: Matche | undefined;
 
   private  routeId: number;
 
+  public  link : string = ""
+  public  shareLinkedin : string = ""
+
 
   constructor(private matcheService: MatcheService,private  route: ActivatedRoute, private  authService: AuthenticationService,private router: Router) {
     this.subscribeOnAuthUser();
     this.routeId = Number(this.route.snapshot.params['id']);
+    this.link = environment.base_api_url+'matches/details/'+this.routeId;
     this.loaddetails(this.routeId)
   }
 
