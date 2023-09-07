6b30e8065c13260e9c41f5a9653b18c0c91479db
ulrich Ganglo <gangloulrichluc@gmail.com>
2023-09-07 19:33:26 +0100

    Team, matches seeders generations

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Matche;
+use App\Models\Team;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -16,11 +18,74 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-       User::create([
-             'name' => 'Administrator',
+        // Set Administrator
+        User::create([
+            'name' => 'Administrator',
             'email' => 'admin@gmail.com',
             'password' => Hash::make('Epitech2023'),
-            'role'=>'admin'
-         ]);
+            'role' => 'admin'
+        ]);
+
+
+        // Create Teams
+
+        Team::create([
+            'name' => 'ALPHA',
+            'coach' => 'Pep GUARDIOLA',
+            'win' => 0,
+            'lost' => 0,
+            'draw' => 0,
+            'points' => 0
+        ]);
+
+        Team::create([
+            'name' => 'BETA',
+            'coach' => 'Massimiliano ALLEGRI',
+            'win' => 0,
+            'lost' => 0,
+            'draw' => 0,
+            'points' => 0
+        ]);
+
+        Team::create([
+            'name' => 'GAMMA',
+            'coach' => 'Thomas TUCHEL',
+            'win' => 0,
+            'lost' => 0,
+            'draw' => 0,
+            'points' => 0
+        ]);
+
+        Team::create([
+            'name' => 'OMEGA',
+            'coach' => 'Luis ENRIQUE',
+            'win' => 0,
+            'lost' => 0,
+            'draw' => 0,
+            'points' => 0
+        ]);
+
+        // get Team Id
+
+        $teamIds = Team::pluck('id')->toArray();
+
+        // Matches combinaisons generations
+
+        $indexHome = 0;
+        $indexAway = $indexHome + 1;
+
+
+        for ($indexHome = 0; $indexHome < count($teamIds); $indexHome++) {
+            for ($indexAway = $indexHome + 1; $indexAway < count($teamIds); $indexAway++) {
+
+                Matche::create([
+                    'home_team' => $teamIds[$indexHome],
+                    'away_team' => $teamIds[$indexAway],
+                    'score_home' => 0,
+                    'score_away' => 0
+                ]);
+
+            }
+        }
     }
 }
